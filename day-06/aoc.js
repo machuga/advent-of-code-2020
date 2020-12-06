9a089a27a21659f0b84d6429d1bac22b110a1805
Matt Machuga <machuga@gmail.com>
2020-12-06 08:48:09 -0500

    Day 06 part 2

--- day-06/aoc.js
+++ day-06/aoc.js
@@ -1,12 +1,40 @@
 const { readFileSync } = require('fs');
 const input = readFileSync(0).toString();
 
-const inputList = input.split('\n\n').map(group =>
+const intersection = (setA, setB) => {
+  console.log(setA, setB);
+  const newSet = new Set()
+
+  setB.forEach((el) => {
+    if (setA.has(el)) {
+      newSet.add(el)
+    }
+  });
+
+  return newSet;
+};
+
+const inputListAny = input.split('\n\n').map(group =>
   Array.from(new Set(
     group.split('\n').reduce((acc, person) =>
       acc.concat(person.split('').filter(Boolean))
       , []).filter(Boolean)
   ))
-).filter(Boolean); // STDIN_FILENO = 0
+).filter(Boolean);
 
-console.log(inputList.reduce((acc, group) => acc + group.length, 0));
+const inputListAll = input.split('\n\n').filter(Boolean).map(group =>
+  Array.from(
+    group.split('\n').reduce((acc, person, i) => {
+      const personSet = new Set(person.split('').filter(Boolean));
+
+      if (i === 0) {
+        return personSet;
+      }
+
+      return intersection(acc, personSet);
+    } , new Set())
+  )
+);
+
+console.log("For groups where anyone answered: ", inputListAny.reduce((acc, group) => acc + group.length, 0));
+console.log("For groups where everyone answered: ", inputListAll.reduce((acc, group) => acc + group.length, 0));
